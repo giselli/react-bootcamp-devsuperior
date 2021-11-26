d39b74ac0c6b31fd2a1b737e6a456ac6a53ab05f
Giselli Hara <gi@hara.ninja>
2021-11-26 18:58:17 -0300

    corrigindo o menu de busca - inacabado

--- frontend/src/pages/Home/Catalog/index.tsx
+++ frontend/src/pages/Home/Catalog/index.tsx
@@ -3,25 +3,11 @@ import './styles.css'
 
 function Catalog() {
   return (
-    <div className="catalog-container container">
-        <div className="pb-5 pt-5 text-center">
-        <div className="row">
-          <div className="col-md-10 col-xl-7 me-auto ms-auto">
-            <div className="col-sm-12 col-lg-9 col-md-10 me-auto ms-auto">
-              <form>
-                <div className="align-items-center g-2 row">
-                  <div className="col-sm">
-                    <input type="text" className="form-control pb-2 pe-3 ps-3 pt-2" placeholder="Digite a sua busca" />
-                  </div>
-                  <div className="col-sm-auto text-end">
-                    <button type="submit" className="btn btn-primary pb-2 pe-4 ps-4 pt-2">
-                      <span className="align-middle">BUSCAR</span>
-                    </button>
-                  </div>
-                </div>
-              </form>
-            </div>
-          </div>
+     <div className="catalog-container container">
+      <div className="catalog-search-container row">
+        <div className="col-12">
+          <input type="text" placeholder="Digite sua busca" />
+          <button className="btn btn-primary">Buscar</button>
         </div>
       </div>
 
