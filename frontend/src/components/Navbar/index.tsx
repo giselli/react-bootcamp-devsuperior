c3917c8f9578237eddc65028e2e72e3c156df5b7
Giselli Hara <gi@hara.ninja>
2021-11-26 15:15:39 -0300

    corrigindo o navbar

--- frontend/src/components/Navbar/index.tsx
+++ frontend/src/components/Navbar/index.tsx
@@ -4,41 +4,39 @@ import 'bootstrap/js/src/collapse.js';
 import { Link, NavLink } from 'react-router-dom';
 
 function Navbar() {
-    return (
-        <nav className="navbar navbar-expand-md navbar-dark main-nav">
-            <div className="container-fluid">
-                <Link to="/" className="nav-logo-text">
-                    <h4>Carros Top</h4>
-                </Link>
+  return (
+    <nav className="navbar navbar-expand-md navbar-dark main-nav">
+      <div className="container-fluid">
+        <Link to="/" className="nav-logo-text">
+          <h4>Carros Top</h4>
+        </Link>
 
-                <button
-                    className="navbar-toggler"
-                    type="button"
-                    data-bs-toggle="collapse"
-                    data-bs-target="#dscatalog-navbar"
-                    aria-controls="dscatalog-navbar"
-                    aria-expanded="false"
-                    aria-label="Toggle navigation"
-                >
-                    <span className="navbar-toggler-icon"></span>
-                </button>
+        <button
+          className="navbar-toggler"
+          type="button"
+          data-bs-toggle="collapse"
+          data-bs-target="#dscatalog-navbar"
+          aria-controls="dscatalog-navbar"
+          aria-expanded="false"
+          aria-label="Toggle navigation"
+        >
+          <span className="navbar-toggler-icon"></span>
+        </button>
 
-                <div className="alinhar">
-                    <div className="collapse navbar-collapse" id="dscatalog-navbar">
-                        <ul className="navbar-nav offset-md-2 main-menu">
-                            <li>
-                                <NavLink to="/" activeClassName="active" exact >Home</NavLink>
-                            </li>
-                            <li>
-                                <NavLink to="/products" activeClassName="active">Catálogo</NavLink>
-                            </li>
-                        </ul>
-                    </div>
-                </div>
-            </div>
-        </nav>
 
-    );
+        <div className="collapse navbar-collapse" id="dscatalog-navbar">
+          <ul className="navbar-nav offset-md-2 main-menu">
+            <li>
+              <NavLink to="/" activeClassName="active" exact >Home</NavLink>
+            </li>
+            <li>
+              <NavLink to="/products" activeClassName="active">Catálogo</NavLink>
+            </li>
+          </ul>
+        </div>
+      </div>
+    </nav>
+  );
 }
 
 export default Navbar;
